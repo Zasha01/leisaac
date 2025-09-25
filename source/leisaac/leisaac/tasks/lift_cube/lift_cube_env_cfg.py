455e78e620da7d844313343ea1c6db3cbf422124
Zakariea Sharfeddine <94124954+Zasha01@users.noreply.github.com>
2025-09-25 14:06:29 +0200

    init with chess table

--- source/leisaac/leisaac/tasks/lift_cube/lift_cube_env_cfg.py
+++ source/leisaac/leisaac/tasks/lift_cube/lift_cube_env_cfg.py
@@ -23,13 +23,23 @@ from ..template import SingleArmTaskSceneCfg, SingleArmTaskEnvCfg, SingleArmTerm
 
 @configclass
 class LiftCubeSceneCfg(SingleArmTaskSceneCfg):
-    """Scene configuration for the lift cube task."""
+    """Scene configuration for the lift task with chess board baked into scene.usd."""
 
     scene: AssetBaseCfg = TABLE_WITH_CUBE_CFG.replace(prim_path="{ENV_REGEX_NS}/Scene")
 
+    # chess board already lives inside the scene.usd → just point to the prim
+    chess: AssetBaseCfg = AssetBaseCfg(
+        prim_path="{ENV_REGEX_NS}/Scene/chess",   # NOTE: if yours is "{ENV}/Scene/chess/chess", change to that
+        spawn=None,                               # important: we are NOT spawning a new USD
+        init_state=AssetBaseCfg.InitialStateCfg(
+            pos=(0.0, 0.0, 0.0),
+            rot=(0.0, 0.0, 0.0, 1.0),
+        ),
+    )
+
     front: TiledCameraCfg = TiledCameraCfg(
         prim_path="{ENV_REGEX_NS}/Robot/base/front_camera",
-        offset=TiledCameraCfg.OffsetCfg(pos=(-0.6, -0.75, 0.38), rot=(0.77337, 0.55078, -0.2374, -0.20537), convention="opengl"),  # wxyz
+        offset=TiledCameraCfg.OffsetCfg(pos=(-0.6, -0.75, 0.38), rot=(0.77337, 0.55078, -0.2374, -0.20537), convention="opengl"),
         data_types=["rgb"],
         spawn=sim_utils.PinholeCameraCfg(
             focal_length=40.6,
@@ -40,7 +50,7 @@ class LiftCubeSceneCfg(SingleArmTaskSceneCfg):
         ),
         width=640,
         height=480,
-        update_period=1 / 30.0,  # 30FPS
+        update_period=1 / 30.0,
     )
 
     light = AssetBaseCfg(
@@ -50,48 +60,34 @@ class LiftCubeSceneCfg(SingleArmTaskSceneCfg):
 
     def __post_init__(self):
         super().__post_init__()
-        delete_attribute(self, 'wrist')
+        delete_attribute(self, "wrist")
+        delete_attribute(self, "cube")  # we don’t use the cube anymore
+
 
 
 @configclass
 class ObservationsCfg(SingleArmObservationsCfg):
-
-    @configclass
-    class SubtaskCfg(ObsGroup):
-        """Observations for subtask group."""
-        pick_cube = ObsTerm(func=mdp.object_grasped, params={"robot_cfg": SceneEntityCfg("robot"), "ee_frame_cfg": SceneEntityCfg("ee_frame"), "object_cfg": SceneEntityCfg("cube")})
-
-        def __post_init__(self):
-            self.enable_corruption = False
-            self.concatenate_terms = False
-
-    # observation groups
-    subtask_terms: SubtaskCfg = SubtaskCfg()
-
     def __post_init__(self):
         super().__post_init__()
-        delete_attribute(self.policy, 'wrist')
+        delete_attribute(self.policy, "wrist")
+        # drop cube-related terms if any
+        if hasattr(self, "subtask_terms"):
+            delete_attribute(self, "subtask_terms")
 
 
 @configclass
 class TerminationsCfg(SingleArmTerminationsCfg):
+    def __post_init__(self):
+        super().__post_init__()
+        if hasattr(self, "success"):
+            delete_attribute(self, "success")
 
-    success = DoneTerm(func=mdp.cube_height_above_base, params={
-        "cube_cfg": SceneEntityCfg("cube"),
-        "robot_cfg": SceneEntityCfg("robot"),
-        "robot_base_name": "base",
-        "height_threshold": 0.20
-    })
 
 
 @configclass
 class LiftCubeEnvCfg(SingleArmTaskEnvCfg):
-    """Configuration for the lift cube environment."""
-
     scene: LiftCubeSceneCfg = LiftCubeSceneCfg(env_spacing=8.0)
-
     observations: ObservationsCfg = ObservationsCfg()
-
     terminations: TerminationsCfg = TerminationsCfg()
 
     def __post_init__(self) -> None:
@@ -100,20 +96,13 @@ class LiftCubeEnvCfg(SingleArmTaskEnvCfg):
         self.viewer.eye = (-0.4, -0.6, 0.5)
         self.viewer.lookat = (0.9, 0.0, -0.3)
 
-        self.scene.robot.init_state.pos = (0.35, -0.64, 0.01)
+        self.scene.robot.init_state.pos = (0.35, -0.64, 0.05)
 
+        # parse subassets from the baked scene (includes chess now)
         parse_usd_and_create_subassets(TABLE_WITH_CUBE_USD_PATH, self)
 
-        domain_randomization(self, random_options=[
-            randomize_object_uniform("cube", pose_range={
-                "x": (-0.075, 0.075), "y": (-0.075, 0.075), "z": (0.0, 0.0),
-                "yaw": (-30 * torch.pi / 180, 30 * torch.pi / 180)}),
-            randomize_camera_uniform("front", pose_range={
-                "x": (-0.005, 0.005), "y": (-0.005, 0.005), "z": (-0.005, 0.005),
-                "roll": (-0.05 * torch.pi / 180, 0.05 * torch.pi / 180),
-                "pitch": (-0.05 * torch.pi / 180, 0.05 * torch.pi / 180),
-                "yaw": (-0.05 * torch.pi / 180, 0.05 * torch.pi / 180)}, convention="opengl"),
-        ])
+
+
 
 
 @configclass
